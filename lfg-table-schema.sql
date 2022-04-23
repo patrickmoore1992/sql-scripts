211f9e31f6386d97790a2d250c3ded2e8e0d91bc
patrickmoore1992 <patrickmoore92@att.net>
2022-04-23 11:41:08 -0500

    Update lfg-table-schema.sql
    formatting

--- lfg-table-schema.sql
+++ lfg-table-schema.sql
@@ -5,21 +5,21 @@ DROP TABLE IF EXISTS lfg.game_types;
 
 -- User accounts
 CREATE TABLE lfg.users (
-	user_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
     create_account_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP    
 );
 
 -- Seed data for users
 INSERT INTO lfg.users (user_id)
 VALUES (1),
-	   (2),
+       (2),
        (3),
        (4),
        (5);
 
 -- Reference table for game types so that there won't be duplicated text values
 CREATE TABLE lfg.game_types (
-	game_type_id INT PRIMARY KEY AUTO_INCREMENT,
+    game_type_id INT PRIMARY KEY AUTO_INCREMENT,
     game_type VARCHAR(50),
     is_enabled TINYINT(1)
 );
@@ -27,12 +27,12 @@ CREATE TABLE lfg.game_types (
 -- Seed data for game types
 INSERT INTO lfg.game_types (game_type, is_enabled)
 VALUES ('Dungeons & Dragons', true),
-	   ('Pathfinder', true),
+       ('Pathfinder', true),
        ('Misc. Board Games', true);
 
 -- Groups created by users
 CREATE TABLE lfg.user_groups (
-	group_id INT PRIMARY KEY AUTO_INCREMENT,
+    group_id INT PRIMARY KEY AUTO_INCREMENT,
     host_user_id INT,
     group_name VARCHAR(200),
     game_type_id INT,
@@ -43,22 +43,22 @@ CREATE TABLE lfg.user_groups (
 
 INSERT INTO lfg.user_groups (host_user_id, group_name, game_type_id)
 VALUES (1, 'DnD test group', 1),
-	   (1, 'Pathfinder test group', 2),
+       (1, 'Pathfinder test group', 2),
        (1, 'Misc. Board Game test group', 3);
        
 -- Roster for user-created groups
 CREATE TABLE lfg.user_group_roster (
-	roster_id INT PRIMARY KEY AUTO_INCREMENT,
+    roster_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT,
     is_host TINYINT(1),
     group_id INT,
-	FOREIGN KEY (user_id) REFERENCES lfg.users(user_id),
+    FOREIGN KEY (user_id) REFERENCES lfg.users(user_id),
     FOREIGN KEY (group_id) REFERENCES lfg.user_groups(group_id)
 );
 
 INSERT INTO lfg.user_group_roster (user_id, is_host, group_id)
 VALUES (1, true, 1),
-	   (2, false, 1),
+       (2, false, 1),
        (3, false, 1),
        (4, false, 1),
        (5, false, 1),
